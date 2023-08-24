bba6f32df4ccf5995742e2a63ce83295f9872701
Keci <dinya.nikitin@gmail.com>
2023-08-24 20:31:48 +0300

    0.1.1

--- public/jsClient/clientPay.js
+++ public/jsClient/clientPay.js
@@ -1,4 +1,33 @@
-function clientPay(chairs, row, chair ){
+function clientPay(chairs, name, chair, hall) {
     let span = document.querySelector('.ticket__chairs');
+    let spanF = document.querySelector('.ticketF__chairs');
+    let value = '';
+    let chairClient = 0;
+    chairs.forEach((el, i) => {
+        if (el === 1) {
+            if (i < chair) {
+                for (let q = 0; q <= i; q++) {
+                    if (!(hall[q] == null)) {
+                        chairClient++;
+                    }
+                }
+                value += `Ряд: 1 Место: ${chairClient} `;
+            }
+            else {
 
+                value += `Ряд: ${(Math.floor(i / chair) + 1)} Место: ${i % chair} `;
+
+            }
+
+        }
+    });
+    span.textContent = value;
+    spanF.textContent = value;
+    document.querySelector('.acceptin-button').addEventListener('click', () => {
+        document.querySelector('.ticket').style.display = 'none';
+        document.querySelector('.ticketF').style.display = 'block';
+    })
+    let src = document.querySelector('.ticket__info-qr').src;
+    src += `${value}`;
+    document.querySelector('.ticket__info-qr').src = src;
 }
--- resources/views/clientPay.blade.php
+++ resources/views/clientPay.blade.php
@@ -9,23 +9,43 @@
 
             <div class="ticket__info-wrapper">
                 <p class="ticket__info">На фильм: <span class="ticket__details ticket__title"> {{ $film->name }}</span></p>
-                <p class="ticket__info">Места: <span class="ticket__details ticket__chairs"></span></p>
+                <p class="ticket__info">Билеты на: <span class="ticket__details ticket__chairs"></span></p>
                 <p class="ticket__info">В зале: <span class="ticket__details ticket__hall">{{ $hall->name }}</span></p>
                 <p class="ticket__info">Начало сеанса: <span
                         class="ticket__details ticket__start">{{ $bookings->time }}</span></p>
                 <p class="ticket__info">Стоимость: <span class="ticket__details ticket__cost">{{ $price }}</span>
                     рублей</p>
-                <button class="acceptin-button" onclick="location.href='ticket.html'" type="button">Получить код
+                <button class="acceptin-button" type="button">Получить код
                     бронирования</button>
                 <p class="ticket__hint">После оплаты билет будет доступен в этом окне. Покажите
                     QR-код нашему контроллёру у входа в зал.</p>
                 <p class="ticket__hint">Приятного просмотра!</p>
             </div>
         </section>
+        <section class="ticketF">
+
+            <header class="tichet__check">
+                <h2 class="ticket__check-title">Электронный билет</h2>
+            </header>
+
+            <div class="ticket__info-wrapper">
+                <p class="ticket__info">На фильм: <span class="ticket__details ticket__title"> {{ $film->name }}</span>
+                </p>
+                <p class="ticket__info">Билеты на: <span class="ticket__details ticket__chairs ticketF__chairs"></span></p>
+                <p class="ticket__info">В зале: <span class="ticket__details ticket__hall">{{ $hall->name }}</span></p>
+                <p class="ticket__info">Начало сеанса: <span
+                        class="ticket__details ticket__start">{{ $bookings->time }}</span></p>
+                <img class="ticket__info-qr"
+                    src='http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl={{ $hall->name }} {{ $film->name }} {{ $bookings->date }} {{ $bookings->time }} '>
+
+                <p class="ticket__hint">Покажите QR-код нашему контроллеру для подтверждения бронирования.</p>
+                <p class="ticket__hint">Приятного просмотра!</p>
+            </div>
+        </section>
     </main>
     <script src="/jsClient/clientPay.js"></script>
     <script>
-        clientPay({{ $chairPay }}, {{ $hall->row }}, {{$hall->chair}})
+        clientPay({{ $chairPay }}, {{ $hall->row }}, {{ $hall->chair }}, {{ $hall->chairs }})
     </script>
     <script src="/jsClient/clientTimeRevers.js"></script>
 @endsection
